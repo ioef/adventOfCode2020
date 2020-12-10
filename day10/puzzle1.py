56fa239136dc52733f05d5cd39d022a1b782ddc5
ioef <iosifidise@gmail.com>
2020-12-10 21:04:01 +0200

    Day 10 part 1

---
+++ day10/puzzle1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+with open("input", "r") as fileIn:
+    jolts = [int(line.strip()) for line in fileIn.readlines()]
+
+
+jolts.sort()
+
+jolts = [0] + jolts
+jolts.append(jolts[-1]+3)
+
+count_diff_of_ones = 0
+count_diff_of_threes = 0
+for i in range(len(jolts)-1):
+        diff = jolts[i+1] - jolts[i]
+
+        if diff == 1:
+            count_diff_of_ones += 1
+        elif diff == 3:
+            count_diff_of_threes += 1
+
+
+print(count_diff_of_ones, count_diff_of_threes)
+print(count_diff_of_ones * count_diff_of_threes)
